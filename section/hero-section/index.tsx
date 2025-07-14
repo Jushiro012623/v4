93c9965c5700af8667aac4963762cfe373848585
Ivan Allen Macabontoc <ivan_dev@my.pisopay.com.ph>
2025-07-14 16:49:56 +0800

    test

--- section/hero-section/index.tsx
+++ section/hero-section/index.tsx
@@ -5,7 +5,7 @@ import TextArea from "./text-area";
 import MyImage from "./my-image";
 const HeroSection = forwardRef(() => {
   return (
-    <section className="w-full h-[90vh] flex flex-col justify-start items-center">
+    <section className="w-full h-screen md:h-[90vh] flex flex-col justify-start items-center">
       <div className="flex flex-col items-center translate-y-1/3 w-full">
         <span className={`block h-9 w-full `} />
         <MyImage />
